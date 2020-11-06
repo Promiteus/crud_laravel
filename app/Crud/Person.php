d9370762d25169a48c80b48bc705e14b758bb7a9
Roman Matveev <romanm87@mail.ru>
2020-11-06 16:05:59 +1000

    6.10.2020 -- подготовка

---
+++ app/Crud/Person.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Crud;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Person
+ * @package App\Crud
+ */
+class Person extends Model
+{
+    //
+    protected $fillable = [
+       "firstName",
+       "lastName",
+       "email",
+       "age"
+    ];
+}
---
+++ app/Http/Controllers/Crud/PersonsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Crud;
+
+use App\Crud\Person;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PersonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['products'] = Person::orderBy('id','desc')->paginate(10);
+        return view('person.list',$data);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('person.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'firstName' => 'required|size:50',
+            'lastName' => 'size:50',
+            'email' => 'required|email',
+            'age' => 'required|min:12|max:70'
+        ]);
+
+        Person::create($request->all());
+        return Redirect::to('persons')
+            ->with('success','Успешно! Человек добавлен.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
