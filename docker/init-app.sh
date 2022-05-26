e6df6a9af0b8a8ca5802d9d59ff1d67d8753c52b
Roman Matveev <r.matveev@socrat.marketing>
2022-05-26 10:50:51 +1000

    26.05.2022 -- Переработка docker контейнров. Создание скрипта начальной инициализации приложения. Расшаривание директории storage.

---
+++ docker/init-app.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+echo 'Installing new dependencies...'
+docker exec -it  crud-php-fpm bash -c "composer install"
+echo 'Clearing cache...'
+docker exec -it  crud-php-fpm bash -c "php artisan cache:clear"
+echo 'Creating new migrations...'
+docker exec -it  crud-php-fpm bash -c "php artisan migrate"
