bcdb4bfb9e78b43f8cca038241dd6a1199ec10ea
Roman Matveev <r.matveev@socrat.marketing>
2022-05-26 12:12:38 +1000

    26.05.2022 -- Вынос конфигураций docker за исходный код. Часть 3. Разрешение переменных окружения вне кода

--- init-app.sh
+++ init-app.sh
@@ -3,5 +3,7 @@ echo 'Installing new dependencies...'
 docker exec -it  crud-php-fpm bash -c "composer install"
 echo 'Clearing cache...'
 docker exec -it  crud-php-fpm bash -c "php artisan cache:clear"
+echo 'Clearing config...'
+docker exec -it  crud-php-fpm bash -c "php artisan config:clear"
 echo 'Creating new migrations...'
 docker exec -it  crud-php-fpm bash -c "php artisan migrate"
---
+++ init-env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+touch .env
+cp src/.env .env
